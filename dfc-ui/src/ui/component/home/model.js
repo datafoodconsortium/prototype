80240dce299fe635b8044bada573d33a02a38343
simon <>
2022-04-13 18:03:39 +0200

    menu, edit product, jsquery test

--- dfc-ui/src/ui/component/home/model.js
+++ dfc-ui/src/ui/component/home/model.js
@@ -3,9 +3,16 @@ import view from 'html-loader!./view.html';
 export default class Home extends GenericElement {
   constructor() {
     super(view);
+    const sr=$(this.shadowRoot);
+    const go = sr.find("#go");
+    console.log(go.get());
+    // go.hide();
+    // this.shadowRoot.querySelector('[name="stockLimitation"]'),
   }
   connectedCallback() {
     super.connectedCallback();
+
+
   }
 
   disconnectedCallback() {
@@ -22,3 +29,26 @@ export default class Home extends GenericElement {
   }
 }
 window.customElements.define('x-home', Home);
+
+
+
+// export default class Menu extends GenericElement {
+//   constructor() {
+//     super(view);
+//   }
+//
+//   connectedCallback() {
+//     super.connectedCallback();
+//     console.log('connectedCallback MENU');
+//   }
+//
+//   disconnectedCallback() {
+//     super.disconnectedCallback();
+//   }
+//
+//   attributeChangedCallback(attrName, oldVal, newVal) {
+//     super.attributeChangedCallback(attrName, oldVal, newVal);
+//   }
+//
+// }
+// window.customElements.define('x-menu', Menu);
