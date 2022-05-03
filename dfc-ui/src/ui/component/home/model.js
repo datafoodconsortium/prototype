c5615c6d8c3d78075366e5f941c956cb9ae207d4
simon <>
2022-05-03 16:02:28 +0200

    home

--- dfc-ui/src/ui/component/home/model.js
+++ dfc-ui/src/ui/component/home/model.js
@@ -20,76 +20,76 @@ export default class Home extends GenericElement {
 
     const sr=$(this.shadowRoot);
 
-    sr.find("#myButton").dxButton({
-      text: "test devExtreme framework",
-      onClick: function() {
-        alert('devExtreme framework is well integrated', '', false);
-      }
-    });
+    // sr.find("#myButton").dxButton({
+    //   text: "test devExtreme framework",
+    //   onClick: function() {
+    //     alert('devExtreme framework is well integrated', '', false);
+    //   }
+    // });
 
-    const employees = [
-          {
-              "id": 1,
-              "parentId": 0,
-              "fullName": "John Heart",
-              "position": "CEO",
-              "email": "jheart@dx-email.com"
-          },
-          {
-              "id": 2,
-              "parentId": 1,
-              "fullName": "Samantha Bright",
-              "position": "COO",
-              "email": "samanthab@dx-email.com"
-          },
-          {
-              "id": 3,
-              "parentId": 1,
-              "fullName": "Arthur Miller",
-              "position": "CTO",
-              "email": "arthurm@dx-email.com"
-          },
-          {
-              "id": 4,
-              "parentId": 1,
-              "fullName": "Robert Reagan",
-              "position": "CMO",
-              "email": "robertr@dx-email.com"
-          },
-          {
-              "id": 5,
-              "parentId": 2,
-              "fullName": "Greta Sims",
-              "position": "HR Manager",
-              "email": "gretas@dx-email.com"
-          }
-      ];
-
-    const widget = this.shadowRoot.querySelector('#widget');
-    // console.log("widget",widget);
-
-
-    new TreeList(this.shadowRoot.getElementById("widget"), {
-    "autoExpandAll": true,
-    "columns": [
-        "position",
-        "fullName",
-        "email"
-    ],
-    "dataSource": employees,
-      "showRowLines": true
-    });
-
-    sr.find("#treeList").dxTreeList({
-        dataSource: employees,
-        "autoExpandAll": true,
-        "columns": [
-            "position",
-            "fullName",
-            "email"
-        ],
-        "showRowLines": true
-    });
+    // const employees = [
+    //       {
+    //           "id": 1,
+    //           "parentId": 0,
+    //           "fullName": "John Heart",
+    //           "position": "CEO",
+    //           "email": "jheart@dx-email.com"
+    //       },
+    //       {
+    //           "id": 2,
+    //           "parentId": 1,
+    //           "fullName": "Samantha Bright",
+    //           "position": "COO",
+    //           "email": "samanthab@dx-email.com"
+    //       },
+    //       {
+    //           "id": 3,
+    //           "parentId": 1,
+    //           "fullName": "Arthur Miller",
+    //           "position": "CTO",
+    //           "email": "arthurm@dx-email.com"
+    //       },
+    //       {
+    //           "id": 4,
+    //           "parentId": 1,
+    //           "fullName": "Robert Reagan",
+    //           "position": "CMO",
+    //           "email": "robertr@dx-email.com"
+    //       },
+    //       {
+    //           "id": 5,
+    //           "parentId": 2,
+    //           "fullName": "Greta Sims",
+    //           "position": "HR Manager",
+    //           "email": "gretas@dx-email.com"
+    //       }
+    //   ];
+    //
+    // const widget = this.shadowRoot.querySelector('#widget');
+    // // console.log("widget",widget);
+    //
+    //
+    // new TreeList(this.shadowRoot.getElementById("widget"), {
+    // "autoExpandAll": true,
+    // "columns": [
+    //     "position",
+    //     "fullName",
+    //     "email"
+    // ],
+    // "dataSource": employees,
+    //   "showRowLines": true
+    // });
+    //
+    // sr.find("#treeList").dxTreeList({
+    //     dataSource: employees,
+    //     "autoExpandAll": true,
+    //     "columns": [
+    //         "position",
+    //         "fullName",
+    //         "email"
+    //     ],
+    //     "showRowLines": true
+    // });
 
 
     let injectedStyle = document.createElement('style');
